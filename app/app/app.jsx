fdcd433b2575731fbebb252ac14f575fe6437689
Akhil Acharya <akhilcacharya@gmail.com>
2016-08-27 19:48:55 -0400

    Refactor

--- app/app/app.jsx
+++ app/app/app.jsx
@@ -1,37 +1,35 @@
 import React from "react";
 import { render } from "react-dom";
-import WebView from 'react-electron-webview'; 
-import { Window, Content, PaneGroup ,Pane } from "react-photonkit";
-import parse from 'url-parse'; 
+import WebView from 'react-electron-webview';
+import { Window } from "react-photonkit";
+import parse from 'url-parse';
 
-const storage = localStorage; 
+const storage = localStorage;
 
-import Footer from "./footer.jsx"; 
-import Sidebar from "./sidebar.jsx";
-import PoliteBarPane from './politebar.jsx'; 
+import PoliteBarPane from './politebar.jsx';
 import FriendPane from './friends.jsx';
 
 require('../index.scss');
 
-const baseUrl = "http://localhost:3000/"
+const baseUrl = "http://localhost:3000/";
 
-const root = document.querySelector("#main"); 
+const root = document.querySelector("#main");
 
 const onFinishLoad = (evt) => {
-    const url = parse(evt.srcElement.src, true); 
+    const url = parse(evt.srcElement.src, true);
     if(url.pathname == "/received"){
-      const token = url.query.token; 
-      storage.setItem('authy', token); 
+      const token = url.query.token;
+      storage.setItem('authy', token);
       render(App(token), root);
     }
 }
- 
+
 const App = (auth) => (
     <Window>
         <PoliteBarPane  auth={auth} fetchURL={baseUrl + 'api/self'} syncURL={baseUrl + 'api/sync'}    />
         <FriendPane     auth={auth} friendURL={baseUrl + 'api/friends'} />
     </Window>
-); 
+);
 
 const Login = () => (
     <Window>
@@ -39,10 +37,10 @@ const Login = () => (
           <WebView didFinishLoad={onFinishLoad} src={baseUrl + 'login'}/>
         </div>
     </Window>
-); 
+);
 
 
-const auth = storage.getItem('authy'); 
+const auth = storage.getItem('authy');
 
 if(auth != null){
   render(App(auth), root);
